4b37f2ad8e90b74b8f9ad0c0b57b798833622f94
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-18 20:32:28 -0500

    create rock paper scissors game

--- sesiones/2-control-flow/main.py
+++ sesiones/2-control-flow/main.py
@@ -1,13 +1,37 @@
-from random import randint 
+import random
 
-print("Bienvenido al juego de adivinar el número. Yo escogeré un número del 1 al 100 y tú tendrás la oportunidad de adivinar.")
-user_input = input("Introduce un número:")
-
-if user_input.isnumeric() == True:
-    computer_guess = randint(1,1)
-    if int(user_input) == computer_guess:
-        print("You guessed correctly!")
+def obtener_opcion_jugador():
+    opcion = input("Elige una opción: Piedra (r), Papel (p) o Tijera (t): ")
+    if opcion.lower() == "r":
+        return "piedra"
+    elif opcion.lower() == "p":
+        return "papel"
+    elif opcion.lower() == "t":
+        return "tijera"
     else:
-        print("Try again!")
-else: 
-    print("Ese no es un número entero.")
+        print("Opción inválida. Por favor, elige nuevamente.")
+        return obtener_opcion_jugador()
+
+def obtener_opcion_computadora():
+    opciones = ["piedra", "papel", "tijera"]
+    return random.choice(opciones)
+
+def determinar_ganador(jugador, computadora):
+    if jugador == computadora:
+        return "Empate"
+    elif (jugador == "piedra" and computadora == "tijera") or \
+         (jugador == "papel" and computadora == "piedra") or \
+         (jugador == "tijera" and computadora == "papel"):
+        return "¡Ganaste!"
+    else:
+        return "¡La computadora ganó!"
+
+def jugar_piedra_papel_tijera():
+    print("¡Bienvenido a Piedra, Papel o Tijera!")
+    jugador = obtener_opcion_jugador()
+    computadora = obtener_opcion_computadora()
+    print("Tu elección: " + jugador)
+    print("Elección de la computadora: " + computadora)
+    print(determinar_ganador(jugador, computadora))
+
+jugar_piedra_papel_tijera()
