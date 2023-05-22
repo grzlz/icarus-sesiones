71c63d89e22215b1ceb131f9811af8824915566f
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-21 20:09:03 -0500

    initiate session 3

---
+++ sesiones/3-control-flow/main.py
@@ -0,0 +1,26 @@
+my_list = [1, 2, 3, 4]
+
+if len(my_list) == 1:
+    print(my_list[0])
+
+if len(my_list) == 2:
+    print(my_list[1])
+
+if len(my_list) == 3:
+    print(my_list[2])
+
+if len(my_list) == 4:
+    print(my_list[3])
+
+
+my_list = ["azul", "amarillo", "amazul", "verde", "rojo"]
+
+# Hacer un print para cada elemento de la lista con ifs
+
+
+# Loops
+
+for i in 
+
+# Funciones
+# Loops con funciones
