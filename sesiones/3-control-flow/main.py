99809a46639e88a45b6d53233449a75c764bb931
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-21 20:13:04 -0500

    print index through range and len of iterable

--- sesiones/3-control-flow/main.py
+++ sesiones/3-control-flow/main.py
@@ -1,26 +1,8 @@
-my_list = [1, 2, 3, 4]
-
-if len(my_list) == 1:
-    print(my_list[0])
-
-if len(my_list) == 2:
-    print(my_list[1])
-
-if len(my_list) == 3:
-    print(my_list[2])
-
-if len(my_list) == 4:
-    print(my_list[3])
-
-
+# Loops
 my_list = ["azul", "amarillo", "amazul", "verde", "rojo"]
 
-# Hacer un print para cada elemento de la lista con ifs
-
-
-# Loops
-
-for i in 
-
+for i in range(len(my_list)):
+    print(i)
+# Entrar al iterador usando len de una lista
 # Funciones
 # Loops con funciones
