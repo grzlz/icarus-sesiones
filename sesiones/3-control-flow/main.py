52254aba159b6f3b86e2ebc98b26029cc2363e64
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-21 20:13:38 -0500

    rewrite original for loop

--- sesiones/3-control-flow/main.py
+++ sesiones/3-control-flow/main.py
@@ -1,8 +1,11 @@
 # Loops
 my_list = ["azul", "amarillo", "amazul", "verde", "rojo"]
 
+for i in my_list:
+    print(i)
+
+# Entrar al iterador usando len de una lista
 for i in range(len(my_list)):
     print(i)
-# Entrar al iterador usando len de una lista
 # Funciones
 # Loops con funciones
