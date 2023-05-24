fc778914689ae8efaf7bdafee39b67cf6986b72c
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-24 15:54:54 -0500

    create play chipiado function

---
+++ tareas/2-tarea/funciones.py
@@ -0,0 +1,26 @@
+
+
+
+def play_chipiado():
+    print("Bievnenido a mi play chipiado")
+
+    try: 
+        user_choice = int(input("¿Qué quieres jugar? Tenemos 1. juego 1, 2. juego 2, 3. juego 3."))
+    except:
+        print("Introduce una opción válida")
+        return "Error"
+
+    if user_choice == 1:
+        print("Escogiste el juego 1")
+
+    elif user_choice == 2:
+        print("Elegiste el juego 2")
+
+    elif user_choice == 3:
+        print("Elegiste el juego 3")
+
+    else: 
+        print("Introduce una opción válida")
+
+
+play_chipiado()
