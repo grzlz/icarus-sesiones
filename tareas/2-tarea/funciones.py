9eebf993fb29f923023b1693d0bc8824bdb363c9
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-24 16:00:51 -0500

    refactor code, put games in games module

--- tareas/2-tarea/funciones.py
+++ tareas/2-tarea/funciones.py
@@ -1,14 +1,3 @@
-from random import randint
-
-def number_guessing():
-    computer_guess = randint(1,1)
-    user_guess = int(input("Select a number"))
-
-    if user_guess == computer_guess:
-        print("You won!")
-    else: 
-        print("You lost")
-
 
 
 def play_chipiado():
