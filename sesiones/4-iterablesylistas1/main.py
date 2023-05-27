b29437bbd90224a55dabb50661b79da62733a532
AndresP <andukial@gmail.com>
2023-05-27 17:38:18 -0600

    Crate a code where the program check a list and if an element is a 0 to move it to the end of the list.

--- sesiones/4-iterablesylistas1/main.py
+++ sesiones/4-iterablesylistas1/main.py
@@ -1,22 +1,8 @@
-def funcion_nombre(nombre):
-    for a in nombre:
-        print(a)
-
-mi_nombre = ['A', 'n', 'd', 'r', 'e', 's']
-
-funcion_nombre(mi_nombre)
-
-diccionario1 = {
-    1: "azul",
-    2: "rojo",
-    3: "verde"
-}
-
-print(diccionario1[4])
-
 lista = [1,2,3,0,4]
 
 for a in lista:
     if a == 0:
         lista.remove(a)
-        lista.insert(x-1, a)
+        lista[-1] = a
+
+print(lista)
