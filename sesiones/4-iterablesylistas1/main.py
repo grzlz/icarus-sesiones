63813a35977efddc3342820e5a216206703b7d00
AndresP <andukial@gmail.com>
2023-05-27 17:54:44 -0600

    create a python function that recieves a number list and then returns all numbers opn the list added.

--- sesiones/4-iterablesylistas1/main.py
+++ sesiones/4-iterablesylistas1/main.py
@@ -1,15 +1,10 @@
 lista = [1,2,3,0,4]
 
-for a in lista:
-    a + 1
-
-print(lista)
-
-def sumar_1(lista):
-    lista2 = []    
+def add_numbers(lista):
+    acumulador = 0
     for a in lista:
-        lista2.append(a+1)
-    return(lista2)
+        acumulador += a
+    print(acumulador)
 
-sumar_1(lista)
-print(sumar_1(lista))   
+add_numbers(lista)
+        
