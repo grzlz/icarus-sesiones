2b706f93a173257a79e880a22f2840621041ff70
AndresP <andukial@gmail.com>
2023-05-27 17:48:31 -0600

    Create a function in python that recieves a number list and then returns each number with a plus one.

--- sesiones/4-iterablesylistas1/main.py
+++ sesiones/4-iterablesylistas1/main.py
@@ -1,8 +1,15 @@
 lista = [1,2,3,0,4]
 
 for a in lista:
-    if a == 0:
-        lista.remove(a)
-        lista[-1] = a
+    a + 1
 
 print(lista)
+
+def sumar_1(lista):
+    lista2 = []    
+    for a in lista:
+        lista2.append(a+1)
+    return(lista2)
+
+sumar_1(lista)
+print(sumar_1(lista))   
