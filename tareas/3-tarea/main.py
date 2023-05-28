64f99cbdf0e41b2eb6c3950da957fa863fd3cf03
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-28 14:46:51 -0500

    create fourth function

--- tareas/3-tarea/main.py
+++ tareas/3-tarea/main.py
@@ -10,3 +10,10 @@ def second_function(my_list):
 # 3. Write a function than prints the length of a list
 def third_function(my_list):
     return len(my_list)
+
+# 4. Write a program that modifies the second item in a list to be "Ola k ase"
+def fourth_function(my_list):
+    my_list[1] = "Ola k ase"
+    return my_list
+
+print(fourth_function(first_function()))
