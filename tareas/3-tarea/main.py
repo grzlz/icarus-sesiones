008437631581f3dcc39f1663d8a2902395c3caaa
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-28 14:39:37 -0500

    create second function

--- tareas/3-tarea/main.py
+++ tareas/3-tarea/main.py
@@ -2,3 +2,9 @@
 
 def first_function():
     return [1, 2, 3, 4, 5]
+
+# 2. Write a function that accesses and prints the third item from the list
+def second_function(my_list):
+    return my_list[2]
+
+print(second_function(first_function()))
