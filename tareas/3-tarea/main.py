12f6b15038cc2af29f95565c98c660586c68a338
grzlz <guillermo.rdzlpz@gmail.com>
2023-05-28 14:41:12 -0500

    create third function

--- tareas/3-tarea/main.py
+++ tareas/3-tarea/main.py
@@ -7,4 +7,6 @@ def first_function():
 def second_function(my_list):
     return my_list[2]
 
-print(second_function(first_function()))
+# 3. Write a function than prints the length of a list
+def third_function(my_list):
+    return len(my_list)
