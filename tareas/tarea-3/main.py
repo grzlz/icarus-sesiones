251039f677bd2d9a18d026d2e112d84396584976
AndresP <andukial@gmail.com>
2023-05-30 18:53:00 -0600

    Arreglé la tarea

--- tareas/tarea-3/main.py
+++ tareas/tarea-3/main.py
@@ -1,17 +1,20 @@
 def paso1():
     lista1 = [1,2,3,4,5]
+    return(lista1)
 
 def paso2():
-    print(lista1[3])
+    print(lista1[2])
 
-def paso3():
+def paso3(lista1):
     print(len(lista1))
 
 def paso4(lista):
     lista[2] = "Saludos cordiales."
+    return(lista)
 
 def paso5(lista, elemento):
     lista.append(elemento)
+    return(lista)
 
 def paso6(lista):
     def split_list(lista):
@@ -22,6 +25,7 @@ def paso6(lista):
 def paso7():
     lista7 = [1,2,3,4,[1,2,3,4]]
     resultado7 =  lista7[4][2]
+    return(lista7)
     print(resultado7)
 
 def paso8(lista):
@@ -43,12 +47,14 @@ def paso10(lista1,lista2):
     return lista10
 
 def paso11(lista):
+    return(sum(lista))
     print(sum(lista))
 
 def paso12(lista):
     acumulador = 1
     for a in lista:
         acumulador *= a 
+    return(acumulador)
     print(acumulador)
 
 def paso13(lista):
@@ -56,11 +62,14 @@ def paso13(lista):
     print(min(lista))
 
 def paso14(lista):
-    lista14 = list(set(lista))
-    return lista14
+    lista14 = []
+    for a in lista:
+        if a not in lista14:
+            lista14.append(a)
+    return(lista14)
 
 def paso15(lista):
     a = lista
     a.reverse()
-    print(a)
+    return(a)
 
