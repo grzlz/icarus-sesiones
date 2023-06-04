28918130a3c77470de5aa3bbcfd0ec0c1e1b5acf
grzlz <guillermo.rdzlpz@gmail.com>
2023-06-03 21:17:07 -0500

    fix spacing

--- tareas/5-tarea/tarea5.py
+++ tareas/5-tarea/tarea5.py
@@ -16,17 +16,26 @@ class Person:
     def birthday(self):
         self.age += 1
 
-yo = Person("Memo", 26)
-print(yo.age)
 # 4. Modify the `Person` class to include an initialization method that assigns `name` and `age` to instances when they're created.
+
 # 5. Create a `Student` class that inherits from `Person` and includes an additional `grade` attribute.
+
 # 6. Override the `birthday` method in `Student` class so that it also prints a congratulatory message.
+
 # 7. Use the `super()` function in the `Student` class to call the `birthday` method of the `Person` class.
+
 # 8. Modify the `Person` class to make the `age` attribute private. Add a method that returns the value of `age`.
+
 # 9. Use property decorators (`@property` and `@<attribute>.setter`) to get and set the value of private attributes in the `Person` class.
+
 # 10. Add a `species` class attribute to the `Person` class and print it for an instance of the class.
+
 # 11. Add a class method that modifies the `species` class attribute and call it on the class.
+
 # 12. Add a static method that returns a greeting message, and call it on an instance of the class.
+
 # 13. Create a `Teacher` class that inherits from both `Person` and a new `Employee` class.
+
 # 14. Implement a method in the `Person`, `Student`, and `Teacher` classes that behaves differently in each class.
+
 # 15. Add a `__str__` method to the `Person` class that returns a string representing a person instance.
