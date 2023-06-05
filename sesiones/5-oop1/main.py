9ed62a9ad3007de1cbbc389cc27359b5797f3949
fjbarragans <franciscojbarragans@gmail.com>
2023-06-04 20:12:17 -0500

    To class Car, instatiate turn on method that print rum rum

--- sesiones/5-oop1/main.py
+++ sesiones/5-oop1/main.py
@@ -1,6 +1,8 @@
 class Carro:
     def __init__(self,cilindros):
         self.cilindros = cilindros
+    def arrancar(self):
+        print("Rum, rum")
 
 mi_carro = Carro(4)
-print(mi_carro.cilindros)
+mi_carro.arrancar()
