42a46dc18e9334032783b117f6fa4449474ac616
AndresP <andukial@gmail.com>
2023-06-04 20:12:28 -0600

    Add to the Car class a method named start that prints rum rum when called

--- sesiones/5-OOP/main.py
+++ sesiones/5-OOP/main.py
@@ -1,6 +1,11 @@
 class Carro:
     def __init__(self,cilindros):
         self.cilindros = cilindros
+    def arrancar(self):
+        print("Rum,Rum")
 
 mi_carro = Carro(4)
 print(mi_carro.cilindros)
+
+mi_carro.arrancar()
+
