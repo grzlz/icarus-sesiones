6d25d7cc7f432ac2006b1ac7569db832ffabe741
AndresP <andukial@gmail.com>
2023-06-06 18:49:13 -0600

    Arregla el último punto.

---
+++ tareas/tarea-5/main.py
@@ -0,0 +1,42 @@
+class Persona:
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad
+    def cumple(self):
+        self.edad += 1 
+
+mi_persona = Persona("Andrés", 21)
+
+class Estudiante(Persona):
+    def __init__(self,nombre,edad,grado):
+        super().__init__(nombre, edad)
+        self.grado = grado
+    def cumple(self):
+        print(f"Felicidades {self.nombre}.")
+    def cumple_Persona(self):
+        print(f"Antes tenía {self.edad}.")
+        super().cumple()
+        print(f"Ahora tiene {self.edad}.")
+
+        
+mi_estudiante = Estudiante("Andrés", 21, "Licenciatura")
+mi_estudiante.cumple_Persona()
+
+class Perro:
+    def __init__(self,nombre,raza):
+        self.nombre = nombre
+        self.raza = raza
+    def ladrar(self):
+        print("Woof")
+
+mi_perro = Perro("Firulais", "Akita")
+
+class Retriever(Perro):
+    def __init__(self,nombre,raza):
+        super().__init__(nombre, raza)
+        self.color = "Amarillo"
+
+balto = Retriever("Firulais2","Retriever")
+print(balto.color)
+
+
