bdcb1d2d15acb08d53bc2713aa2d492552029b89
AndresP <andukial@gmail.com>
2023-06-06 18:41:36 -0600

    puse las carpetas en su respectivo lugar

--- sesiones/5-OOP/main.py
+++ sesiones/5-OOP/main.py
@@ -9,3 +9,14 @@ print(mi_carro.cilindros)
 
 mi_carro.arrancar()
 
+
+class Personajes:
+    def __init__(self, bando, presistencia,pataque):
+        self.bando = bando
+        self.presistencia = resistencia
+        self.pataque = ataque
+    def ataque(self):
+        print(f"Ataca con {self.pataque}")
+    
+mi_zombie = Personajes(zombies,10,5)
+mi_zombie.ataque
