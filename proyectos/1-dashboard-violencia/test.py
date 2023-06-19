a8ab3b6775db20344f2284a4050fb35c51d323bb
grzlz <guillermo.rdzlpz@gmail.com>
2023-06-19 02:45:47 -0600

    transform into a function

--- proyectos/1-dashboard-violencia/test.py
+++ proyectos/1-dashboard-violencia/test.py
@@ -4,64 +4,59 @@ import requests
 from io import BytesIO
 # What happens if I make a request?
 
-url = "http://www.informeseguridad.cns.gob.mx/files/homicidios_13062023_v2.pdf"
 
-response = requests.get(url)
+def get_dict(date):
+    url = f"http://www.informeseguridad.cns.gob.mx/files/homicidios_{date}_v2.pdf"
+    response = requests.get(url)
+    pdf_content = response.content
 
-print(response.status_code)
-pdf_content = response.content
-
-with pdfplumber.open(BytesIO(pdf_content)) as pdf:
-    # Perform operations on the PDF document
-    # Access pages, extract text, extract tables, etc.
-    for page in pdf.pages:
-    # Loop through each page in the PDF
+    with pdfplumber.open(BytesIO(pdf_content)) as pdf:
         first_page = pdf.pages[0]
         text = first_page.extract_text()
-        print(len(text))
 
+    pattern = r'([a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+)\s+(\d+)'
+    matches = re.findall(pattern, text)
+    result = {match[0].strip(): int(match[1]) for match in matches}
+    states = [
+        'Aguascalientes',
+        'Baja California',
+        'Baja California Sur',
+        'Campeche',
+        'Coahuila',
+        'Colima',
+        'Chiapas',
+        'Chihuahua',
+        'Ciudad de México',
+        'Durango',
+        'Guanajuato',
+        'Guerrero',
+        'Hidalgo',
+        'Jalisco',
+        'México',
+        'Michoacán',
+        'Morelos',
+        'Nayarit',
+        'Nuevo León',
+        'Oaxaca',
+        'Puebla',
+        'Querétaro',
+        'Quintana Roo',
+        'San Luis Potosí',
+        'Sinaloa',
+        'Sonora',
+        'Tabasco',
+        'Tamaulipas',
+        'Tlaxcala',
+        'Veracruz',
+        'Yucatán',
+        'Zacatecas'
+    ]
 
-    # Process each line (excluding the header line and the last line)
-pattern = r'([a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+)\s+(\d+)'
+    new_dict = {key: result[key] for key in states if key in result}
 
-matches = re.findall(pattern, text)
-result = {match[0].strip(): int(match[1]) for match in matches}
+    print(new_dict)
 
-states = [
-    'Aguascalientes',
-    'Baja California',
-    'Baja California Sur',
-    'Campeche',
-    'Coahuila',
-    'Colima',
-    'Chiapas',
-    'Chihuahua',
-    'Ciudad de México',
-    'Durango',
-    'Guanajuato',
-    'Guerrero',
-    'Hidalgo',
-    'Jalisco',
-    'México',
-    'Michoacán',
-    'Morelos',
-    'Nayarit',
-    'Nuevo León',
-    'Oaxaca',
-    'Puebla',
-    'Querétaro',
-    'Quintana Roo',
-    'San Luis Potosí',
-    'Sinaloa',
-    'Sonora',
-    'Tabasco',
-    'Tamaulipas',
-    'Tlaxcala',
-    'Veracruz',
-    'Yucatán',
-    'Zacatecas'
-]
+dates = ["10062023", "11062023", "12062023", "13062023"]
 
-new_dict = {key: result[key] for key in states if key in result}
-
-print(new_dict)
+for date in dates:
+    get_dict(date)
