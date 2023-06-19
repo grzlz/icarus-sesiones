7cadcb815b7878fdaf0739454ce1b43f8500718b
grzlz <guillermo.rdzlpz@gmail.com>
2023-06-19 02:33:17 -0600

    do get request to url

--- proyectos/1-dashboard-violencia/test.py
+++ proyectos/1-dashboard-violencia/test.py
@@ -1,22 +1,30 @@
 import pdfplumber
 import re
+import requests
+# What happens if I make a request?
 
-with pdfplumber.open("C:/Users/grzlz/Desktop/homicidios_14062023_v2.pdf") as pdf:
-    # Initialize an empty list to hold all lines
+url = "http://www.informeseguridad.cns.gob.mx/files/homicidios_13062023_v2.pdf"
 
-    # Loop through each page in the PDF
-    first_page = pdf.pages[0]
-    text = first_page.extract_text()
-    print(len(text))
+response = requests.get(url)
 
-    # Initialize an empty dictionary
-    dictionary = {}
+print(response.status_code)
 
-    # Process each line (excluding the header line and the last line)
-pattern = r'([a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+)\s+(\d+)'
+# with pdfplumber.open("C:/Users/grzlz/Desktop/homicidios_14062023_v2.pdf") as pdf:
+#     # Initialize an empty list to hold all lines
 
-matches = re.findall(pattern, text)
-result = {match[0].strip(): int(match[1]) for match in matches}
+#     # Loop through each page in the PDF
+#     first_page = pdf.pages[0]
+#     text = first_page.extract_text()
+#     print(len(text))
+
+#     # Initialize an empty dictionary
+#     dictionary = {}
+
+#     # Process each line (excluding the header line and the last line)
+# pattern = r'([a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+)\s+(\d+)'
+
+# matches = re.findall(pattern, text)
+# result = {match[0].strip(): int(match[1]) for match in matches}
 
 # print(result)
 
@@ -24,41 +32,41 @@ result = {match[0].strip(): int(match[1]) for match in matches}
 
 
 
-states = [
-    'Aguascalientes',
-    'Baja California',
-    'Baja California Sur',
-    'Campeche',
-    'Coahuila',
-    'Colima',
-    'Chiapas',
-    'Chihuahua',
-    'Ciudad de México',
-    'Durango',
-    'Guanajuato',
-    'Guerrero',
-    'Hidalgo',
-    'Jalisco',
-    'México',
-    'Michoacán',
-    'Morelos',
-    'Nayarit',
-    'Nuevo León',
-    'Oaxaca',
-    'Puebla',
-    'Querétaro',
-    'Quintana Roo',
-    'San Luis Potosí',
-    'Sinaloa',
-    'Sonora',
-    'Tabasco',
-    'Tamaulipas',
-    'Tlaxcala',
-    'Veracruz',
-    'Yucatán',
-    'Zacatecas'
-]
+# states = [
+#     'Aguascalientes',
+#     'Baja California',
+#     'Baja California Sur',
+#     'Campeche',
+#     'Coahuila',
+#     'Colima',
+#     'Chiapas',
+#     'Chihuahua',
+#     'Ciudad de México',
+#     'Durango',
+#     'Guanajuato',
+#     'Guerrero',
+#     'Hidalgo',
+#     'Jalisco',
+#     'México',
+#     'Michoacán',
+#     'Morelos',
+#     'Nayarit',
+#     'Nuevo León',
+#     'Oaxaca',
+#     'Puebla',
+#     'Querétaro',
+#     'Quintana Roo',
+#     'San Luis Potosí',
+#     'Sinaloa',
+#     'Sonora',
+#     'Tabasco',
+#     'Tamaulipas',
+#     'Tlaxcala',
+#     'Veracruz',
+#     'Yucatán',
+#     'Zacatecas'
+# ]
 
-new_dict = {key: result[key] for key in states if key in result}
+# new_dict = {key: result[key] for key in states if key in result}
 
-print(new_dict)
+# print(new_dict)
