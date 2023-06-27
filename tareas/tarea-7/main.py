17bfb2f1ed8553d1b9d48955bfa523baeaf75bc6
AndrésP <andukial@gmail.com>
2023-06-27 05:12:47 -0600

    1. Realiza una investigación y encuentra 5 APIs públicas diferentes. Anota la URL base

--- tareas/tarea-7/main.py
+++ tareas/tarea-7/main.py
@@ -1,14 +1,24 @@
 import requests
 import pandas as pd
+import json
 
-#https://pokeapi.co/api/v2/
-#https://jsonplaceholder.typicode.com/posts'
+#https://pokeapi.co/api/v2/pokemon/ditto
+#https://api.spotify.com/v1/recommendations
 #https://api.covidtracking.com
 #https://api.usaspending.gov/api/v2/agency/012/
 #https://datos.gob.mx/contratacionesabiertas/
 
-response = requests.get("https://pokeapi.co/api/v2/")
-print('Status Code:', response.status_code)
-print ("Headers:")
-for header, value in response.headers.items():
-    print(header + ":", value)
+response1 = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
+response2 = requests.get("https://api.spotify.com/v1/recommendations")
+response3 = requests.get("https://api.covidtracking.com")
+response4 = requests.get("https://api.usaspending.gov/api/v2/agency/012/")
+response5 = requests.get("https://datos.gob.mx/contratacionesabiertas/")
+
+lista_response = [response1,response2,response3,response4,response5]
+
+for r in lista_response:
+    print("Status Code:", r.status_code)
+    print("Headers:", r.headers)
+
+
+
